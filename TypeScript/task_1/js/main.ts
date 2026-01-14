dd422456902ee2a15e08c8cf27844bda42c94d1f
Tomlena-tech <decourttthomas@gmail.com>
2026-01-14 15:06:11 +0100

    Add starting file task_2

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,3 +6,23 @@ interface Teacher {
     location: string,
     contract?: boolean,
 }
+
+const teacher3: Teacher = {
+  firstName: 'Greg',
+  fullTimeEmployee: false,
+  lastName: 'Sala',
+  location: 'London',
+  contract: false,
+};
+interface Directors extends Teacher {
+    numberOfReports: number,
+    }
+
+const director1: Directors = {
+  firstName: 'Philippe',
+  lastName: 'Lapique',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
