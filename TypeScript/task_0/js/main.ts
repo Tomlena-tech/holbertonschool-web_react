1f04284e23586e0c3433d95eae30a27bb36925f4
Tomlena-tech <decourttthomas@gmail.com>
2026-01-14 12:55:39 +0100

    Add interface create Students 1/2 + Array

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,21 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'Philippe',
+    lastName: 'Martin',
+    age: 33,
+    location: 'Canada'
+};
+
+const student2: Student = {
+    firstName: 'Sophie',
+    lastName: 'Durand',
+    age: 21,
+    location: 'France'
+};
+const studentsList: Student[] = [student1, student2];
