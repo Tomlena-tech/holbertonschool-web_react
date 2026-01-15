518b061880a03b69d3933ac0a589eb4f0d1e5175
Tomlena-tech <decourttthomas@gmail.com>
2026-01-15 15:16:06 +0100

    Add crud.d.ts

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,5 @@
+import {RowID, RowElement} from './interface';
+
+export function insertRow(row: Rowelement): void;
+export function deleteRow(rowid: RowID): void;
+export function updateRow(rowid: RowID, row: Rowelement): void;
